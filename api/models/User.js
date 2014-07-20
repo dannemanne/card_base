8d0208d1e4c5c2f24aacb0a175ce7456a674497f
Daniel Wiklund <daniel@happyrabbit.com>
2014-07-20 23:55:13 +0800

    Added config

---
+++ api/models/User.js
@@ -0,0 +1,36 @@
+/**
+ * User
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+  	/* e.g.
+  	nickname: 'string'
+  	*/
+    firstName: {
+      type: 'string'
+    },
+
+    lastName: {
+      type: 'string'
+    },
+
+    email: {
+      type: 'string',
+      required: true
+    },
+
+    encryptedPassword: {
+      type: 'string',
+      required: true
+    }
+
+  }
+
+};
