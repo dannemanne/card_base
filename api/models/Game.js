fbd3463a56c2a7c4c78ea4e9a3be67db162dc616
Daniel Wiklund <daniel@happyrabbit.com>
2014-07-24 23:37:04 +0800

    Added list of games

--- api/models/Game.js
+++ api/models/Game.js
@@ -33,6 +33,9 @@ module.exports = {
       type: 'integer',
       defaultsTo: 4,
       required: true
+    },
+    allowedActions: {
+      type: 'array'
     }
   }
 
--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -6,8 +6,10 @@ angular.module('cardBase.controllers', [])
   .controller('HomeCtrl', ['$scope', function($scope) {
 
   }])
-  .controller('GamesCtrl', ['$scope', function($scope) {
-
+  .controller('GamesCtrl', ['$scope', '$http', function($scope, $http) {
+    $http.get('games').success(function(data){
+      $scope.games = data;
+    });
   }])
   .controller('ContactCtrl', ['$scope', function($scope) {
 
